b766b1513f242cbd968347b965298a67c846af4e
Patryk Tchórzewski <patryk@dark-side.pro>
2025-08-04 13:05:12 +0200

    fix plugin load

---
+++ src/DependencyInjection/DarkSideProSyliusFacebookFeedExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DarkSidePro\SyliusFacebookFeedPlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class DarkSideProSyliusFacebookFeedExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
